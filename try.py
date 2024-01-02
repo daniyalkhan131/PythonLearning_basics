f4f4c626ab1bf6cb0cb1c022080da216353f21d5
daniyalkhan131 <daniyalkhan131@gmail.com>
2024-01-02 16:02:11 +0530

    adding try.py file

---
+++ try.py
@@ -0,0 +1,16 @@
+# import pathlib
+
+# d=pathlib.Path(__file__)
+# print(d)
+# print(type(d))
+# print(d.as_posix())
+# print(type(d.as_posix()))
+
+# print(d.parent)
+
+
+import sys
+print(type(sys.argv[1]))
+print(sys.argv[1])
+print(sys.argv[0])
+print(type(sys.argv[0]))
